bb8c0730c134c48b0330460bc811fd9087b9bede
mohuandao <1034255194@qq.com>
2018-04-07 22:55:25 +0800

    xxx

--- src/main/java/com/example/holiday/service/UserService.java
+++ src/main/java/com/example/holiday/service/UserService.java
@@ -4,9 +4,12 @@ import com.example.holiday.model.SysUser;
 
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 
 public interface UserService {
     void addUser(SysUser user);
     List<LinkedHashMap<String,Object>> select_me();
+
+    Map<String,Object> login(String name,String password);
 }
