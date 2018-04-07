09852e72bca4b0f921a9cb6979a3974ca0b27070
mohuandao <1034255194@qq.com>
2018-04-07 22:54:52 +0800

    xxx

---
+++ src/main/java/com/example/holiday/mapper/HUserMapper.java
@@ -0,0 +1,10 @@
+package com.example.holiday.mapper;
+
+import com.example.holiday.model.HUser;
+import com.example.holiday.utils.MyMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HUserMapper extends MyMapper<HUser> {
+    HUser selectByName(String name);
+}
