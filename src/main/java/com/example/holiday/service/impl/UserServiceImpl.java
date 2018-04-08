8741e66f2df0e546cef688d52bdaaebcdd2450db
mohuandao <1034255194@qq.com>
2018-04-08 19:52:22 +0800

    4.8

--- src/main/java/com/example/holiday/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/holiday/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.holiday.service.impl;
 import com.example.holiday.mapper.HUserMapper;
 import com.example.holiday.mapper.SysUserMapper;
 import com.example.holiday.model.HUser;
+import com.example.holiday.model.LoginTicket;
 import com.example.holiday.model.SysUser;
 import com.example.holiday.service.UserService;
 import com.example.holiday.utils.HolidayUtils;
@@ -10,10 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -50,20 +48,36 @@ public class UserServiceImpl implements UserService {
         HUser user = hUserMapper.selectByName(name);
         if (user == null) {
             map.put("msg","用户名不存在");
+            return map;
         }
-        //密码强度
+        if (!HolidayUtils.MD5(password).equals(user.getuPassword())) {
+            map.put("msg","密码不正确");
+            return map;
+        }
+        /*//密码强度
         user = new HUser();
         user.setuName(name);
         //也可以密码加上盐后生产md5
-        user.setuPassword(HolidayUtils.MD5(password));
+        user.setuPassword(HolidayUtils.MD5(password));*/
         //登录
-
+        String ticket = addTicket(user.getuId());
+        map.put("ticket",ticket);
+        map.put("userId",user.getuId());
         return map;
     }
 
+    //产生ticket
+    private String addTicket(int uid) {
+        LoginTicket ticket = new LoginTicket();
+        ticket.setuId(uid);
+        Date date = new Date();
+        date.setTime(date.getTime()+1000*3600*24);//24小时后过期
+        ticket.setExpired(date);
+        ticket.setStatus(0);
+        ticket.setTicket(UUID.randomUUID().toString().replace("-",""));
+        return ticket.getTicket();
+    }
 
-    /*public void ss() {
-        userMapper.selectAll();
-    }*/
+
 
 }
