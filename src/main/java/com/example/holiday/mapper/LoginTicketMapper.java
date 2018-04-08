d0670427379b88201babbef097b3cc00cc0b9ef4
mohuandao <1034255194@qq.com>
2018-04-08 19:53:19 +0800

    4.8

---
+++ src/main/java/com/example/holiday/mapper/LoginTicketMapper.java
@@ -0,0 +1,9 @@
+package com.example.holiday.mapper;
+
+import com.example.holiday.model.LoginTicket;
+import com.example.holiday.utils.MyMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoginTicketMapper extends MyMapper<LoginTicket> {
+}
