beaa7b0a11aa5e292f2f9fbba4316ddeb6a6e6b8
mohuandao <1034255194@qq.com>
2018-04-10 18:05:09 +0800

    4.10

---
+++ src/main/java/com/example/holiday/mapper/CProjectMapper.java
@@ -0,0 +1,9 @@
+package com.example.holiday.mapper;
+
+import com.example.holiday.model.CProject;
+import com.example.holiday.utils.MyMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CProjectMapper extends MyMapper<CProject> {
+}
