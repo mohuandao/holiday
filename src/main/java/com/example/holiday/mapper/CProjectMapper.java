feaa2deb3e628356b7a675bfb8d3a8c2fab0970f
mohuandao <1034255194@qq.com>
2018-04-11 20:48:39 +0800

    4.10

--- src/main/java/com/example/holiday/mapper/CProjectMapper.java
+++ src/main/java/com/example/holiday/mapper/CProjectMapper.java
@@ -4,6 +4,10 @@ import com.example.holiday.model.CProject;
 import com.example.holiday.utils.MyMapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Map;
+
 @Repository
 public interface CProjectMapper extends MyMapper<CProject> {
+    List<Map<String,Object>> list();
 }
