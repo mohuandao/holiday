12c873cf7356222e4527b4c427cd22b92d7fc62f
mohuandao <1034255194@qq.com>
2018-05-04 16:50:39 +0800

    加了后台校验,避免错误的参数去请求后台

--- src/main/java/com/example/holiday/controller/SpiderController.java
+++ src/main/java/com/example/holiday/controller/SpiderController.java
@@ -1,29 +1,57 @@
 package com.example.holiday.controller;
 
 import com.example.holiday.utils.station.spiderUtils;
+import com.example.holiday.utils.station.station_name;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Controller
 public class SpiderController {
-
+    //跳转到查询页
+    @RequestMapping("/station")
+    public String station() {
+        return "station";
+    }
+    /**
+     * 接口
+     * @param from_station
+     * @param to_station
+     * @param date
+     * @return
+     */
     @RequestMapping("/getInfo")
     @ResponseBody
-    public List getInfo(@RequestParam String from_station,
-                        @RequestParam String to_station,
-                        @RequestParam String date) {
+    public Map getInfo(@RequestParam String from_station,
+                       @RequestParam String to_station,
+                       @RequestParam String date) throws ParseException {
         List<Object> list = new ArrayList<>();
+        //String today = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        Date today = new Date();
+        Date v_date = new SimpleDateFormat("yyyy-MM-dd").parse(date);
         try {
-             list = spiderUtils.getINfo(from_station, to_station, date);
+            //做一个判断,输入信息正常时才调用方法
+             if (null == station_name.getCodeByCity(from_station) | null == station_name.getCodeByCity(to_station)
+                     | v_date.before(today)) {
+                 list = null;
+             }else {
+                 list = spiderUtils.getINfo(from_station, to_station, date);
+             }
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
-        return list;
+        //返回表格接受的数据
+        HashMap<Object, Object> map = new HashMap<>();
+        map.put("code","0");
+        map.put("msg","");
+        map.put("count",1000);
+        map.put("data",list);
+        return map;
     }
 }
